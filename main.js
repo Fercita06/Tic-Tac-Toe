69c2d6390e51bf8c7abb33facdd431756f0b77ef
suarezfer1712 <suarezfer1712@gmail.com>
2022-05-31 14:45:50 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,91 @@
+const tiles = document.querySelectorAll(".container__tile");
+console.log(tiles);
+const buttonNew = document.getElementById("button-new");
+const buttonReset = document.getElementById("button-reset");
+
+// assign a value to each player.
+
+let player = "X";
+
+
+// create an array to store all the moves done by both players
+let player1 = [];
+let player2 = [];
+let allInputs = [];
+
+/*indexes within the board
+[1,2,3],
+[4,5,6],
+[7,8,9],
+*/
+
+const winningConditions = [
+  ["1","2","3"],
+  ["4","5","6"],
+  ["7","8","9"],
+  ["3","6","9"],
+  ["1","4","7"],
+  ["2","5","8"],
+  ["7","5","3"],
+  ["1","5","9"],
+
+];
+
+tiles.forEach((tile, index) => {
+  tile.addEventListener("click", (event) => {
+    console.log(event.target.innerHTML);
+    // tiles.value = event.target.innerText;
+    if (player === "X"){
+      tile.innerText="x";
+      console.log(event.target);
+      tile.classList.add ("player1");
+      allInputs.push(event.target.id)
+      player1.push(event.target.id);
+      player = "O";
+      checkWinner(player1, "player1");
+    }else {
+      tile.innerHTML = "O";
+      tile.classList.add ("player2");
+      allInputs.push(event.target.id);
+      player2.push(event.target.id);
+      checkWinner(player2, "player2");
+      player = "X";
+      
+    }
+    
+  }) 
+});
+
+const resetGame = () => {
+  player1=[];
+  player2=[];
+  allInputs=[]; 
+  player = "X";
+
+  tiles.forEach((tile) => {
+   tile.innerText = ""; 
+});
+
+}
+
+const checkWinner = (player,playerName) =>{
+ let isPlayerWinning = winningConditions.map(condition => condition.every(number => (player.includes(number)) ));
+ console.log(isPlayerWinning);
+ 
+  if(isPlayerWinning.includes(true)){
+    alert(`${playerName} won`);
+    resetGame();
+  }else if(!isPlayerWinning.includes(true) && allInputs.length == 9){
+    alert("Nobody won");
+  }
+
+}
+
+buttonReset.addEventListener("click", resetGame);
+
+
+
+
+
+
+
